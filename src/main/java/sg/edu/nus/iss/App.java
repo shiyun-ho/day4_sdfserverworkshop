6b93cce679fc06825da8ce39ace5d1274db2507f
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-17 14:41:38 +0800

    server side settled

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,6 +1,15 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Hello world!
@@ -12,8 +21,10 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
+     * @throws FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         String dirPath = "data2";
         String fileName = "";
 
@@ -26,6 +37,36 @@ public final class App {
             System.out.println("Directory already exists");
         } else {
             newDirectory.mkdir();
+        }
+
+        //read cookie file
+
+        Cookie cookie = new Cookie();
+        cookie.readCookieFile();
+        cookie.showCookies();
+
+        //establish server socket
+
+        ServerSocket ss = new ServerSocket(7000); 
+        Socket s = new ss.accept(); //establish connection and waiting for client to connect
+
+        try (InputStream is = s.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is); 
+            DataInputStream dis = new DataInputStream(bis);
+            String msgReceived = " ";
+
+            while (!msgReceived.equals("close")){
+                msgReceived = dis.readUTF(); //reads utf 
+                //clients receive get cookie
+
+                if (msgReceived.equalsIgnoreCase("get-cookie")){
+                    String cookieValue = cookie.returnCookie(); 
+                }
+
+            }
+        } catch (EOFException ex){
+            s.close(); //close the second
+            ss.close(); //closes the first server socket 
         }
 
     }
--- src/main/java/sg/edu/nus/iss/Cookie.java
+++ src/main/java/sg/edu/nus/iss/Cookie.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Cookie {
-    String dirPath = "\\data2";
+    String dirPath = "data2";
     //instantiate a file/ directory object
     String fileName = "cookie.txt";
 

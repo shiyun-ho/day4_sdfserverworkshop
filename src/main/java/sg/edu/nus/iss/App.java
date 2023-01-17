0d906abe64b2306948b9942438effdbe81132705
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-17 15:47:51 +0800

    final version

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,16 @@
 package sg.edu.nus.iss;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -50,27 +53,48 @@ public final class App {
         //establish server socket
 
         ServerSocket ss = new ServerSocket(7000); 
-        Socket s = new ss.accept(); //establish connection and waiting for client to connect
+        Socket s = ss.accept(); //establish connection and waiting for client to connect
+        
 
+        //input stream is at the server, we define a stream which allows an input
         try (InputStream is = s.getInputStream()) {
+            //needs a buffer from input stream
             BufferedInputStream bis = new BufferedInputStream(is); 
+            //data input stream to read 
             DataInputStream dis = new DataInputStream(bis);
-            String msgReceived = " ";
+            String msgReceived = "";
 
-            while (!msgReceived.equals("close")){
-                msgReceived = dis.readUTF(); //reads utf 
-                //clients receive get cookie
+            try(OutputStream os = s.getOutputStream()){
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos); 
+                
+                //wait for data
+                while (!msgReceived.equals("close")){
+                    //when msg is received, read  as utf 
+                    msgReceived = dis.readUTF(); //reads utf
+                    //clients receive get cookie
+                    //if it is get cookie, return cookie value
+                    if (msgReceived.equalsIgnoreCase("get-cookie")){
+                        String cookieValue = cookie.returnCookie();
+                        System.out.println(cookieValue); 
+                        
+                        dos.writeUTF(cookieValue);
+                        dos.flush();
+                    }
 
-                if (msgReceived.equalsIgnoreCase("get-cookie")){
-                    String cookieValue = cookie.returnCookie();
-                    System.out.println(cookieValue); 
+                    
+
                 }
+            
+                dos.close();
+                bos.close();
+                os.close();
 
-            }
+            
         } catch (EOFException ex){
             s.close(); //close the second
             ss.close(); //closes the first server socket 
         }
 
     }
-}
+}}
